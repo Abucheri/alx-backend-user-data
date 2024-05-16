5c1dc400df201d173206055e665637623d58ca55
O'Brien <abucheriobrien@gmail.com>
2024-05-17 00:07:41 +0300

    Regex-ing

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Filtered Logger Module
+"""
+
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """
+    Obfuscates specified fields in a log message.
+
+    Args:
+        fields: A list of strings representing all fields to obfuscate.
+        redaction: A string representing by what the field will be obfuscated.
+        message: A string representing the log line.
+        separator: A string representing by which character is separating all
+                   fields in the log line (message).
+    Returns:
+        str: A string with specified fields obfuscated
+    """
+    for field in fields:
+        message = re.sub(f'{field}=(.*?){separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message
