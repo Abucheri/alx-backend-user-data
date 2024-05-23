d38b6a55611d59b3170784c69f75496912c8b6a0
O'Brien <abucheriobrien@gmail.com>
2024-05-24 02:28:41 +0300

    Logout

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -80,3 +80,30 @@ class SessionAuth(Auth):
 
         # Retrieve the User instance from the database using the user ID
         return User.get(user_id)
+
+    def destroy_session(self, request=None):
+        """
+        Deletes the user session / logout.
+
+        Args:
+            request (flask.Request): The Flask request object.
+
+        Returns:
+            bool: True if the session is successfully destroyed,
+                  False otherwise.
+        """
+        if request is None:
+            return False
+
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return False
+
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return False
+
+        # Delete the session ID from the dictionary
+        del self.user_id_by_session_id[session_id]
+
+        return True
