5e9c566ed31e733eb6d868cdba7a0fbd9d12253f
O'Brien <abucheriobrien@gmail.com>
2024-05-23 23:05:50 +0300

    Et moi et moi et moi!

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -74,6 +74,8 @@ def before_request_handler() -> str:
     if auth.authorization_header(request) is None:
         abort(401)
 
+    request.current_user = auth.current_user(request)
+
     if auth.current_user(request) is None:
         abort(403)
 
