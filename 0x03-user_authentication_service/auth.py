fe678aab3e801c2b9dfa0ac6501f95a94b6260d5
O'Brien <abucheriobrien@gmail.com>
2024-05-30 07:27:19 +0300

    Credentials validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -56,3 +56,21 @@ class Auth:
 
         hashed_password = _hash_password(password)
         return self._db.add_user(email, hashed_password)
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        Validates user login credentials.
+
+        Args:
+            email (str): The email of the user.
+            password (str): The password of the user.
+
+        Returns:
+            bool: True if the login credentials are valid, False otherwise.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            hashed_password = user.hashed_password
+            return bcrypt.checkpw(password.encode(), hashed_password)
+        except NoResultFound:
+            return False
