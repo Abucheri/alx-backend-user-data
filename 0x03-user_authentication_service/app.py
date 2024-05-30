6478cb8f3c94b2d4df777814d47dd640f374eb64
O'Brien <abucheriobrien@gmail.com>
2024-05-30 07:08:49 +0300

    Register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,10 +3,12 @@
 Flask app module
 """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
@@ -18,6 +20,24 @@ def welcome():
         dict: JSON payload with a welcome message.
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    """
+    Route for registering a new user.
+
+    Returns:
+        dict: JSON payload with the result of the registration attempt.
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except (Exception, ValueError):
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
